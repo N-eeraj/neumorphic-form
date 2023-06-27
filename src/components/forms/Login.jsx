92317c7668930311df96bc276d4b5078932d94c4
N-eeraj <neeraj7rajeena@gmail.com>
2023-06-27 22:59:50 +0530

    Added form components template & form styles

--- src/components/forms/Login.jsx
+++ src/components/forms/Login.jsx
@@ -1,9 +1,30 @@
+import formStyles from '@styles/form.module.css'
+
 const Login = () => {
   return (
-    <div>
-      Login
-      <input />
-    </div>
+    <>
+      <h3 className={formStyles.heading}>
+        Login
+      </h3>
+
+      <div className={formStyles.inputContainer}>
+        <input type="text" />
+        <input type="password" />
+      </div>
+
+      <div className={formStyles.actionsContainer}>
+        <button>
+          Login
+        </button>
+
+        <div className={formStyles.switchText}>
+          Don't have an account?
+          <span className={formStyles.switchButton}>
+            Sign Up
+          </span>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/forms/Register.jsx
+++ src/components/forms/Register.jsx
@@ -1,9 +1,31 @@
+import formStyles from '@styles/form.module.css'
+
 const Register = () => {
   return (
-    <div>
-      Register
-      <input />
-    </div>
+    <>
+      <h3 className={formStyles.heading}>
+        Register
+      </h3>
+
+      <div className={formStyles.inputContainer}>
+        <input type="text" />
+        <input type="password" />
+        <input type="password" />
+      </div>
+
+      <div className={formStyles.actionsContainer}>
+        <button>
+          Register
+        </button>
+
+        <div className={formStyles.switchText}>
+          Already have an account?
+          <span className={formStyles.switchButton}>
+            Login
+          </span>
+        </div>
+      </div>
+    </>
   )
 }
 
