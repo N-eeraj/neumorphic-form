98d35860d1a1e9e231f87917627b026c8782b07a
N-eeraj <neeraj7rajeena@gmail.com>
2023-06-27 22:45:14 +0530

    Added app styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import RegisterForm from '@components/forms/Register'
 
 import { preventSubmit } from '@/utils'
 
+import appStyles from '@styles/app.module.css'
+
 const App = () => {
   const [formType, setFormType] = useState('login')
 
   return (
-    <div>
-      <form onSubmit={preventSubmit}>
+    <div className={appStyles.mainBackground}>
+      <form className={appStyles.form} onSubmit={preventSubmit}>
         {formType === 'login' ? <LoginForm /> : <RegisterForm />}
       </form>
     </div>
